8b628fe84cd38d170c69173151d80559687d37fe
chojunsu <ikinye86@gmail.com>
2019-05-09 14:06:24 +0900

    gesture update

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
       home: MyHomePage(title: 'UDB list'),
     );
@@ -28,15 +29,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   var date = "20180101";
-
   var data;
-
   TextEditingController controller = TextEditingController();
 
   Future<String> getJSONData() async {
-    var url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=430156241533f1d058c603178cc3ca0e&targetDt="+ date;
+    var url =
+        "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=430156241533f1d058c603178cc3ca0e&targetDt=" +
+            date;
     var response = await http
         .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
     setState(() {
@@ -65,79 +65,87 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: new Container(
-       child: new Column(
-         children: <Widget>[
-           Padding(
-             padding: const EdgeInsets.all(8.0),
-             child: TextField(
-               onChanged: (value) {
-               },
-               controller: controller,
-               keyboardType:TextInputType.number,
-               decoration: InputDecoration(
-                   labelText: "날짜",
-                   hintText: "YYYYMMDD 로 입력하세요 20180101",
-                   prefixIcon: Icon(Icons.search),
-                   border: OutlineInputBorder(
-                       borderRadius: BorderRadius.all(Radius.circular(25.0)))),
-             ),
-           ),
-           Expanded(
-             child:  new ListView.builder(
-                 itemCount: data == null ? 0 : data.length,
-                 itemBuilder: (BuildContext context, int index) {
-                   return new Container(
-                     child: new Center(
-                       child: new Column(
-                         crossAxisAlignment: CrossAxisAlignment.stretch,
-                         children: <Widget>[
-                           new Card(
-                             child: new Container(
-                               decoration: BoxDecoration(
-                                   border: Border.all(style: BorderStyle.solid),
-                                   borderRadius:
-                                   BorderRadius.all(Radius.circular(25.0))),
-                               child: new Row(
-                                 children: <Widget>[
-                                   new Padding(padding: EdgeInsets.only(right: 20.0) , child:
-                                   new Icon(Icons.movie , color: Colors.blueAccent, size:  75,),),
-                                   new Column(
-                                     children: <Widget>[
-                                       new Text(
-                                         getMovieName(data[index]['movieNm']),
-                                         textAlign: TextAlign.center,
-                                         style: new TextStyle(
-                                             fontSize: 20.0, color: Colors.black),
-                                       ),
-                                       new Text(
-                                         '관객수 : ' + data[index]['audiAcc'] + '명',
-                                         style: new TextStyle(
-                                             fontSize: 20.0,
-                                             color: Colors.lightBlueAccent),
-                                       ),
-                                       new Text(
-                                         getUBD(data[index]['audiAcc']),
-                                         style: new TextStyle(
-                                             fontSize: 20.0,
-                                             color: Colors.lightBlueAccent),
-                                       )
-                                     ],
-                                   )
+        child: new Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                onChanged: (value) {},
+                controller: controller,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "날짜",
+                    hintText: "YYYYMMDD 로 입력하세요 20180101",
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(25.0)))),
+              ),
+            ),
+            Expanded(
+              child: new ListView.builder(
+                  itemCount: data == null ? 0 : data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return GestureDetector(
+                        child: new Container(
+                      child: new Center(
+                        child: new Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            new Card(
+                              child: new Container(
+                                child: new Row(
+                                  children: <Widget>[
+                                    new Padding(
+                                      padding: EdgeInsets.only(right: 20.0),
+                                      child: new Icon(
+                                        Icons.movie,
+                                        color: Colors.blueAccent,
+                                        size: 75,
+                                      ),
+                                    ),
+                                    new Column(
+                                      children: <Widget>[
+                                        new Text(
+                                          getMovieName(data[index]['movieNm']),
+                                          textAlign: TextAlign.center,
+                                          style: new TextStyle(
+                                              fontSize: 20.0,
+                                              color: Colors.black),
+                                        ),
+                                        new Text(
+                                          '관객수 : ' +
+                                              data[index]['audiAcc'] +
+                                              '명',
+                                          style: new TextStyle(
+                                              fontSize: 20.0,
+                                              color: Colors.lightBlueAccent),
+                                        ),
+                                        new Text(
+                                          getUBD(data[index]['audiAcc']),
+                                          style: new TextStyle(
+                                              fontSize: 20.0,
+                                              color: Colors.lightBlueAccent),
+                                        )
+                                      ],
+                                    )
+                                  ],
+                                ),
+                                padding: EdgeInsets.all(15.0),
+                              ),
+                              shape: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(25.0))),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ) , onTap: () => Scaffold.of(context).showSnackBar(SnackBar(content: Text(data[index]['movieNm']) , duration: Duration(seconds: 2),)),);
+                  }),
+            )
+          ],
+        ),
+      ),
 
-                                 ],
-                               ),
-                               padding: EdgeInsets.all(15.0),
-                             ),
-                           )
-                         ],
-                       ),
-                     ),
-                   );
-                 }),
-           )
-         ],
-       ),
-      ) ,
       floatingActionButton: FloatingActionButton(
         onPressed: _searchMovieList,
         tooltip: 'Increment',
@@ -146,10 +154,17 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  String getMovieName(String name){
-    if(name.length > 11){
-      return name.substring(0,10) + "...";
-    }else{
+  _clickItem(BuildContext context, String name) {
+//    AlertDialog dialog = new AlertDialog(
+//        title: new Text('UBD List'), content: new Text('hello'));
+//    showDialog(context: context, builder: (BuildContext context) => dialog);
+    print(name);
+  }
+
+  String getMovieName(String name) {
+    if (name.length > 11) {
+      return name.substring(0, 10) + "...";
+    } else {
       return name;
     }
   }
